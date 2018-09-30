1b1b095e3a69293878417ddc594320c3056aa513
Justin Capalbo <capalbo.justin@gmail.com>
2018-09-30 19:07:24 -0400

    Add component

---
+++ src/components/presentational/Input.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Input = (props) => {
+    let { 
+        label, 
+        text, 
+        type, 
+        id, 
+        value, 
+        handleChange 
+    } = props;
+
+    return (<div className='form-group'>
+        <label htmlFor={label}>{text}</label>
+        <input
+            type={type}
+            className='form-control'
+            id={id}
+            value={value}
+            onChange={handleChange}
+            required
+        />
+    </div>);
+};
+
+Input.propTypes = {
+    label: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    id: PropTypes.sstring.isRequired,
+    value: PropTypes.string.isRequired,
+    handleChange: PropTypes.func.isRequired,
+};
+
+export default Input;
